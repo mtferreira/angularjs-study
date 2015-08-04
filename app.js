777ccabdca1e3127725a4276bcab3373ce802879
Matheus Ferreira <matheus.ferreirraa@gmail.com>
2015-08-04 01:51:29 -0300

    master: requisições ajax com resource

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // criando um module para a palicação e pasando o module nativo ngRoute como depemdência
-$app = angular.module("app",["ngRoute"]);
+$app = angular.module("app", ["ngRoute","ngResource"]);
 
 // configurando as rotas
-$app.config(function($routeProvider){
+$app.config(function($routeProvider) {
   $routeProvider
-  .when('/',{controller:'listController',templateUrl:'list.html'})
-  .when('/edit/:name',{controller:'editController',templateUrl:'form.html'})
-  .when('/new',{controller:'newController',templateUrl:'form.html'})
-  .otherwise({redirectTo:'/'});
+    .when('/', {
+      controller: 'listController',
+      templateUrl: 'list.html'
+    })
+    .when('/edit/:name', {
+      controller: 'editController',
+      templateUrl: 'form.html'
+    })
+    .when('/new', {
+      controller: 'newController',
+      templateUrl: 'form.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 });
 
 // setando o valor de fruits para todos os templates reendenizados
-$app.run(function($rootScope){
-  $rootScope.fuits = ['banana','apple','orange'];
+$app.run(function($rootScope) {
+  $rootScope.fuits = ['banana', 'apple', 'orange'];
 });
--- controllers.js
+++ controllers.js
@@ -1,46 +1,76 @@
 // controller teste, com parametro usando nome que eu quiser
-$app.controller("testeController",['$scope',function($joao){
+$app.controller("testeController", ['$scope', function($joao) {
   $joao.firstName;
 }]);
 
-$app.controller("listController",function($scope) {
+$app.controller("listController", function($scope) {
 
 });
 
-$app.controller("editController",function($scope, $location, $routeProvider){
+$app.controller("editController", function($scope, $location, $routeProvider) {
   $scope.title = "Edit Fruit";
   $scope.fruit = $routeParams.name;
 
   $scope.fruitIndex = $scope.fruits.indexOf($scope.fruit);
-  $scope.save = function(){
-      $scope.fruits[$scope.fruitIndex] = $scope.fruit;
-      $location.path('/');
+  $scope.save = function() {
+    $scope.fruits[$scope.fruitIndex] = $scope.fruit;
+    $location.path('/');
   };
 });
 
-$app.controller("newController",function($scope,$location){
+$app.controller("newController", function($scope, $location) {
   $scope.title = "New controller";
   $scope.fruit = "";
 
-$scope.save = function(){
-      $scope.fruits.push(angular.copy($scope.fruit));
-      $location.path('/');
+  $scope.save = function() {
+    $scope.fruits.push(angular.copy($scope.fruit));
+    $location.path('/');
   };
 });
 
-
-$app.controller("appController",function($scope, $http){
+$app.controller("appController", function($scope, $http) {
   $scope.fruits = [];
 
-  $scope.getData  = function(){
+  $scope.getData = function() {
     $http.get("request/listFruits.html")
-      .success(function(data){
+      .success(function(data) {
         $scope.fruits = data.fruits;
         console.log($scope.fruits);
       })
-      .error(function(data){
+      .error(function(data) {
         alert("Error...");
         console.log(data);
+      });
+  }
+});
+
+$app.controller("phoneController", function($scope, $resource) {
+  Phone = $resource("/phones/:phoneId");
+
+  $scope.getPhoneById = function() {
+    Phone.get({
+      phoneId: $scope.idPhone
+    }, function(data) {
+      $scope.phone = data;
     });
   }
+
+  $scope.getPhones = function() {
+    Phone.query(function(data) {
+      $scope.phones = data;
+    });
+  }
+
+  $scope.savePhone = function() {
+    p = new Phone();
+    p.number = "1111-2222";
+    p.$save();
+  }
+
+  $scope.deletePhone = function() {
+    Phone.delete({
+      phoneId: 10
+    });
+  }
+
 });
